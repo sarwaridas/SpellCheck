af54c24306fe3106488b5a7cbb44c262d936cfb5
sarwaridas <89522502+sarwaridas@users.noreply.github.com>
2021-09-06 17:19:59 -0400

    Fixed punctuation issues

--- nlp_trial2.py
+++ nlp_trial2.py
@@ -1,35 +1,61 @@
+"""
+Build from scratch a spelling corrector in Python. It should include:
+1. tokenization
+2. Levenshtein distance-based non-word spelling correction
+3. de-tokenization
+
+As an example use case, consider a version of Jane Austen’s Sense and Sensibility (available via nltk’s gutenberg corpus) corrupted by random insertions,
+deletions, and substitutions. See for reference gen_corrupted.py.
+Your spelling correction function:
+• should accept a document as a single string and return the corrected
+document as a single string.
+• may use only standard libraries and numpy.
+• may use this reference word list [https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt]
+ English word list, which is ordered by decreasing frequency
+
+"""
+
+from os import PRIO_PGRP
 import time
 import re
 import numpy as np
 
 
 def createADictionary(filename):
-    # Reads txt file (Input exact path of file!)
-    # Returns dictionary of all words contained in the file
+    """
+    Reads txt file and returns dictionary of all words contained
+    in that file, with their frequency [tokenization]
+    """
     word_dict = dict()
     with open(filename, "r") as f:
         for line in f:
             for word in line.strip("\n,.:;-'").lower().split(" "):
-                if word not in word_dict:
-                    word_dict[word] = 1
+                if word.strip("\n,.:;-'") not in word_dict:
+                    word_dict[word.strip("\n,.:;-'")] = 1
                 else:
-                    word_dict[word] += 1
+                    word_dict[word.strip("\n,.:;-'")] += 1
     return word_dict
 
 
-with open("referenceDict.txt", "r") as f:
+with open(
+    "referenceDict.txt", "r"
+) as f:  # Creating a dictionary of reference word list
     referenceList = [i for j in f for i in j.split()]
 
-referenceList1 = np.array(referenceList)
-austenDict = createADictionary("Austen_trial.txt")
+referenceList = np.array(referenceList)
+austenDict = createADictionary(
+    "Austen_trial.txt"
+)  # Creating a dictionary of words in Jane Austen's Sense & Sensibility
 
 
 def calculateEditDistance(source, target):
-    # print(target)
+    """
+    calculates Levenshtein based edit distance
+    between a source and target variable
+    """
     n = len(source)
     m = len(target)
     source = " " + source
-    # print(source)
     target = " " + target
     D = [[0 for i in range(m + 1)] for j in range(n + 1)]
     # Initialization:the zeroth column is the distance from the empty string
@@ -46,7 +72,11 @@ def calculateEditDistance(source, target):
     return D[i][j]
 
 
-def getReplaceWord(string1, reference=referenceList1):
+def getReplaceWord(string1, reference=referenceList):
+    """
+    From the reference list, for any given string, returns the words with the
+    smallest Levenshtein based edit distance
+    """
     replace = string1
     min_dist = len(string1)
     for i in range(len(referenceList) - 1):
@@ -59,30 +89,66 @@ def getReplaceWord(string1, reference=referenceList1):
 
 
 def keepThis(str):
+    """
+    In my code, I don't attempt to correct proper nouns or any words that have a
+    capitalized first letter, unless they are preceded by a period (indicating that they are
+    the start of a sentence). This function returns a True for words I am attempting correct
+    and False if I am skipping over correction
+
+    """
     pattern = '(^[^A-Z0-90-9.,;""?():-_][^A-Z0-9.,;""?():-_]+)'
+    # pattern = "(^[^A-Z0-90-9.,;?():-_][^A-Z0-9.,;?():-_]+)|[.][A-Z].+"
     if re.fullmatch(pattern, str):
         return True
     else:
         return False
 
 
+# using keepThis to check if I am attempting to correct the word
+# using the referenceList to check if the word is already valid, in which case I skip over it
+# finding the replacement word with least edit distance using getReplaceWord()
+
 for key, value in austenDict.items():
+
     if (keepThis(key) == True) and (key not in referenceList):
         austenDict[key] = getReplaceWord(key)
 
+# creating a list of all corrupted words
+corruptedAustenList = np.array([])
+for key, values in austenDict.items():
+    if key.strip("\n,.:;-'") not in referenceList:
+        corruptedAustenList = np.append(corruptedAustenList, key.strip("\n,.:;-'"))
 
+# preserving the punctuation for each corrupted word to instert after replacement
+def keepPunc(x):
+    if len(re.findall(r"[?!,.;:]$", x)) > 0:
+        return re.findall(r"[?!,.;:]$", x)[0]
+    else:
+        return ""
+
+
+# reading trial text and replacing corrupted words with replacement words
+# performing de-tokenization and saving corrected text in 'final_list'
 with open("Austen_trial.txt", "r") as f:
-    data = f.read().replace("\n", " ").split(" ")
+    # data = f.read().replace("\n", " ").split(" ")
+    data = f.read().replace("\n", " ")
     final_list = " "
-    for word in data:
-        if word == "":
-            pass
-        elif (
-            re.findall(r"\w+", word)[0] in austenDict.keys()
-            and type(austenDict[re.findall(r"\w+", word)[0]]) == str
-        ):  ##figure out a way to preserve punctuation
-            final_list = final_list + austenDict[re.findall(r"\w+", word)[0]] + " "
+    for i in data.split(" "):
+        if (
+            i.strip("\n,.:;'") in corruptedAustenList
+            and len(re.findall(r"\w", i)) != 0
+            and re.findall(r"\w+", i)[0] in austenDict.keys()
+            and type(austenDict[re.findall(r"\w+", i)[0]]) != int
+        ):
+            final_list = (
+                final_list
+                + austenDict[re.findall(r"\w+", i.strip("\n,.:;'"))[0]]
+                + keepPunc(i)
+                + " "
+            )
         else:
-            final_list = final_list + word + " "
+            final_list = final_list + i + " "
 
-print(final_list)
+# writing corrected text to "Austen_trial_corrected.txt"
+with open("Austen_trial_corrected.txt", "w") as stream:
+    stream.write(final_list)
